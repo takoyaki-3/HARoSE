4903ce3765523055441d6449c2c6cceb72d0c1e2
takoyaki-3 <mail@takoyaki3.com>
2022-02-16 11:51:09 +0900

    geojsonを外部モジュールに

--- exec/server.go
+++ exec/server.go
@@ -18,35 +18,8 @@ import (
 	"github.com/takoyaki-3/go-gtfs/pkg"
 	"github.com/takoyaki-3/goraph"
 	"github.com/takoyaki-3/goraph/geometry"
+	. "github.com/takoyaki-3/go-geojson"
 )
-
-type Geometry struct {
-	Type        string      `json:"type"`
-	Coordinates interface{} `json:"coordinates"`
-	Properties map[string]string `json:"properties"`
-}
-type Feature struct {
-	Type       string            `json:"type"`
-	Geometry   Geometry          `json:"geometry"`
-	Properties map[string]string `json:"properties"`
-}
-type FeatureCollection struct {
-	Type     string    `json:"type"`
-	Features []Feature `json:"features"`
-	Properties map[string]string `json:"properties"`
-}
-func NewFeatureCollection()*FeatureCollection{
-	fc := new(FeatureCollection)
-	fc.Type = "FeatureCollection"
-	return fc
-}
-func NewLineString(coordinates [][]float64,props map[string]string)*Geometry{
-	geom := new(Geometry)
-	geom.Type = "LineString"
-	geom.Coordinates = coordinates
-	geom.Properties = props
-	return geom
-}
 
 type MTJNodeStr struct {
 	Id    string  `json:"id"`
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/artonge/go-csv-tag/v2 v2.0.7
 	github.com/google/uuid v1.3.0
 	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/takoyaki-3/go-geojson v0.0.1 // indirect
 	github.com/takoyaki-3/go-gtfs v1.1.22
 	github.com/takoyaki-3/go-json v0.0.0-20211221023225-18c8e7ac7ccb
 	github.com/takoyaki-3/goraph v0.0.1
